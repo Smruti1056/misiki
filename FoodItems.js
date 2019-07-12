4de0500872e341cd02bf5e799d464b362e3948bd
smruti <smruti1056@gmail.com>
2019-07-12 23:37:10 +0530

    2nd commit

--- FoodItems.js
+++ FoodItems.js
@@ -1,13 +1,14 @@
 //fetching the api with the fetch ---the change is "the fech is being replaced with the axios api"
 
 import React , { Component } from 'react' ;
-import {Dimensions, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import {Dimensions,ScrollView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export default class FoodItems extends Component {
  
@@ -37,35 +38,92 @@ export default class FoodItems extends Component {
       const stock = this.state.stock;
       const time = this.state.time;
       const type = this.state.type;
+
+      // IF condition
+    const food_Veg = <Image 
+    style={{height:40,width:40,marginRight:0}}
+    source={{uri :  'https://www.misiki.in/veg.png'}}/>;
+
+    const food_Nonveg = <Image style={{height:40,width:40,marginRight:0}}
+    source={{uri : 'https://www.misiki.in/non-veg.png'}}/>;
+
+
+    let food;
+    if (this.state.type == 'V'){
+      food = food_Veg
+    } else {
+      food = food_Nonveg
+    }
       
       
         return (
           
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ScrollView style={styles.container}>
+             <View style={styles.top}>
                
                <Image style={styles.image1} source={{uri: img}} resizeMode='contain'/>
-               <Text>{name}</Text>
-               <Text>{rate}</Text>
-               <Text>{restaurant}</Text>
-               <Text>{stock}</Text>
-               <Text>{time}</Text>
-               <Text>{type}</Text>
-                             
-                <TouchableOpacity style={styles.inc_style} onPress={this.increment} >
-                      <Icon name={'add'}  size={20}  /> 
-               </TouchableOpacity>
-                
-                <Text>no of items={this.state.no_of_items}</Text>
+               
+               
+               <View style={styles.top1}>
+               <Text style={styles.slugStyle}>
+                      {restaurant}     
+                </Text>
+                <Text style={styles.stockStyle}>
+                            Only {stock} left
+                          </Text>
+                </View>
+                <View style={styles.center}>
+                 
+                 <Text style={styles.chocoStyle}>
+                            {name}    
+                  </Text>
+                 
+                         
+                  <View style={styles.buttom}>  
+                      <View style={styles.imagelogo}>
+                         {food}  
+                     </View>
 
-                {this.state.no_of_items > 0 ? <TouchableOpacity style={styles.dec_style} onPress={this.decrement}>
-                     <Icon name={'remove'}  size={20}  />
-               </TouchableOpacity> : null }
+                    
+                      
+                  </View>
+                  <View style={styles.amountStyle}>
+                  <Text style={styles.rateStyle}>
+                       INR: {rate}
+                     </Text>
+                     
+                     
+                     
+                     {/* <View style={styles.amount1Style}> */}
+                            <TouchableOpacity style={styles.inc_style} onPress={this.increment} >
+                                <Icon name={'add'}  size={20}  /> 
+                            </TouchableOpacity>
+                
+                            <Text>no of items={this.state.no_of_items}</Text>
+
+                          {this.state.no_of_items > 0 ? <TouchableOpacity style={styles.dec_style} onPress={this.decrement}>
+                          <Icon name={'remove'}  size={20}  />
+                          </TouchableOpacity> : null }
+                    {/* </View> */}
+               </View>
+                 <Text style={styles.timeStyle}>
+                 Expected Delivery:{time}
+                  </Text>
+                                       
+                                              
+                 </View>          
+               
+               
+               
+               {/* <Text>{type}</Text> */}
+                             
+                
                 
                 
            
       
-                                       
-          </View>
+               </View>                        
+               </ScrollView>
         );
             }
 
@@ -135,16 +193,93 @@ const styles = StyleSheet.create({
       fontSize: 25,
       textAlign: 'center'
     },
+    container:{
+      flex:1,
+    },
+    top:{
+      height:'35%',
+      // width:'100%',
+    },
+    amountStyle:{
+      margin: '10%',
+      alignItems:'flex-end',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      
+      
+    },
+   
+    slugStyle:{
+    
+      fontSize:18,
+      padding:20,
+      textAlign:"left",
+      color:'red',
+      
+    
+  },
+  rateStyle:{
+    fontSize:20,
+    textAlign:'center',
+  },
+  center:{
+    height:'100%',
+    width:'100%',
+    // backgroundColor: 'grey',
+    // justifyContent:'center'
+    // backgroundColor:'blue',
+  },
+  top1:{
+    height:'10%',
+    // width:'100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    // backgroundColor: 'grey',
+  },
+  stockStyle:{
+    fontSize:15,
+    padding:20,
+    textAlign:"right",
+     alignItems:"flex-end",
+  },
     image1:{
       height: 300,
       width: '100%',
+      flex:1,
+        },
+        timeStyle:{
+          fontSize:15,
+          padding:10,
+          textAlign:'center',
+       
+        },
+        imagelogo:{
+  
+          width: SCREEN_WIDTH * 0.2,
+          height: SCREEN_HEIGHT * 0.3,
+        
+        },
+        
+        buttom:{
+          height:'10%',
+          alignItems:'flex-end',
+          // backgroundColor: 'grey',
+         
+          // backgroundColor: 'red' , 
+        },
+        chocoStyle: {
+          fontSize:25,
+          padding:20,
+          color:'black',
+          fontWeight: 'bold',
         },
     inc_style: {
         borderWidth:1,
         borderColor:'rgba(0,0,0,0.2)',
         alignItems:'flex-end',
         
-        justifyContent:'center',
+        
+        justifyContent:'flex-end',
         width:30,
         height:30,
         backgroundColor:'#fff',
@@ -153,9 +288,9 @@ const styles = StyleSheet.create({
       dec_style: {
           borderWidth:1,
           borderColor:'rgba(0,0,0,0.2)',
-          alignItems:'center',
-         
-          justifyContent:'center',
+          alignItems:'flex-end',
+        
+          justifyContent:'flex-end',
           width:30,
           height:30,
           backgroundColor:'#fff',
