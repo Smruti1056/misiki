3be333bb2ef8f8654796bd21fff5c9b9bb653eeb
smruti <smruti1056@gmail.com>
2019-07-13 03:18:15 +0530

    Adding input field and ordering button

--- FoodItems.js
+++ FoodItems.js
@@ -1,10 +1,11 @@
 //fetching the api with the fetch ---the change is "the fech is being replaced with the axios api"
 
 import React , { Component } from 'react' ;
-import {Dimensions,ScrollView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import {Dimensions,ScrollView, TextInput,StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { bold } from 'ansi-colors';
 
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -66,7 +67,7 @@ export default class FoodItems extends Component {
                
                <View style={styles.top1}>
                <Text style={styles.slugStyle}>
-                      {restaurant}     
+                      By {restaurant}     
                 </Text>
                 <Text style={styles.stockStyle}>
                             Only {stock} left
@@ -87,42 +88,65 @@ export default class FoodItems extends Component {
                     
                       
                   </View>
+                  </View>
                   <View style={styles.amountStyle}>
-                  <Text style={styles.rateStyle}>
-                       INR: {rate}
+                  
+                     
+                     
+                     
+                <View style={styles.amount1Style}>
+                     <Text style={styles.rateStyle}>
+                     ₹ {rate}
                      </Text>
-                     
-                     
-                     
-                     {/* <View style={styles.amount1Style}> */}
-                            <TouchableOpacity style={styles.inc_style} onPress={this.increment} >
+                     </View>
+                            <View style={styles.amount2Style}>
+                              <View style={styles.incStyle}>
+                              {this.state.no_of_items < 3 ? <TouchableOpacity style={styles.inc_style} onPress={this.increment} >
                                 <Icon name={'add'}  size={20}  /> 
-                            </TouchableOpacity>
-                
-                            <Text>no of items={this.state.no_of_items}</Text>
+                            </TouchableOpacity> : null }
+                            
+                            
+                            <Text>{this.state.no_of_items}</Text>
 
                           {this.state.no_of_items > 0 ? <TouchableOpacity style={styles.dec_style} onPress={this.decrement}>
                           <Icon name={'remove'}  size={20}  />
                           </TouchableOpacity> : null }
-                    {/* </View> */}
+                          
+                          </View>
+                    </View>
                </View>
                  <Text style={styles.timeStyle}>
-                 Expected Delivery:{time}
+                 Expected Delivery: {time}
                   </Text>
-                                       
-                                              
-                 </View>          
-               
-               
-               
-               {/* <Text>{type}</Text> */}
-                             
-                
-                
-                
-           
-      
-               </View>                        
+
+                <View style={styles.inputField}>
+                  <View style={{paddingTop:5,paddingRight:4}}>
+                  <TextInput style={styles.nameField}
+                  placeholder="  First Name"
+                  onChangeText={(first_name) => this.setState({first_name})}
+                  />
+                  </View>
+                  <View style={{paddingTop:5, paddingLeft:3}}>
+                  <TextInput style={styles.nameField}
+                  placeholder="  Last Name"
+                  onChangeText={(last_name) => this.setState({last_name})}
+                  />
+                  </View>
+                </View>
+                <View style={styles.qr_Field}>
+                 <TextInput style={styles.qr_no_Field}
+                  placeholder=" Your QrNo"
+                  onChangeText={(qr_no) => this.setState({qr_no})}
+                  />
+                  </View>
+                  <TouchableOpacity style={{paddingTop: 17}}>
+                         
+                         <View style={styles.order_button}><Text style={{fontWeight: 'bold', color: 'pink',fontSize: 20}}>Place Order</Text></View> 
+                            
+
+                          
+                          </TouchableOpacity>                         
+                 </View>                            
                </ScrollView>
         );
             }
@@ -139,49 +163,32 @@ export default class FoodItems extends Component {
                 qr_no: '',
                 isLoading: true,
                 search: '',
-                
-              };
-         
-                                 
-              
+                          };
             }
-          
-          
-
           componentDidMount() {
             const itemId = this.props.navigation.getParam('itemId', 'NO-ID');
-                              
-                   
-                          axios.get(`https://www.misiki.in/api/foods/${itemId}`)
+                 axios.get(`https://www.misiki.in/api/foods/${itemId}`)
                 .then((response) => {
                  
                   this.setState({
                     isLoading: false,
-                  _id: response.data["_id"],
-                  img: response.data["img"],
-                  name: response.data["name"],
-                  rate: response.data["rate"],
-                  restaurant: response.data["restaurant"],
-                  stock: response.data["stock"],
-                  time: response.data["time"],
-                  type: response.data["type"],
+                    _id: response.data["_id"],
+                    img: response.data["img"],
+                    name: response.data["name"],
+                    rate: response.data["rate"],
+                    restaurant: response.data["restaurant"],
+                    stock: response.data["stock"],
+                    time: response.data["time"],
+                    type: response.data["type"],
                          })
-                        
-                      })
-                   
-                   .catch((error) => {
+                     })
+                     .catch((error) => {
                      console.error(error);
                    });
-
-                 
                }
-
-               
-
-    }
+      }
   
     const AppStackNavigator = createStackNavigator({
-        
         FoodItems: { screen: FoodItems}
       },
      );
@@ -196,41 +203,57 @@ const styles = StyleSheet.create({
     container:{
       flex:1,
     },
+    inputField:{
+      flexDirection: 'row',
+      },
     top:{
-      height:'35%',
+      height:'30%',
       // width:'100%',
     },
-    amountStyle:{
-      margin: '10%',
-      alignItems:'flex-end',
+    qr_Field:{
+      alignContent: 'center',
       flexDirection: 'row',
-      flexWrap: 'wrap',
-      
-      
+      paddingTop: 10,
     },
-   
+    nameField:{
+      height:SCREEN_HEIGHT * 0.045,borderWidth:0.5,borderColor:'grey',fontSize:20,width: SCREEN_WIDTH * 0.47,left: 3
+    },
+    qr_no_Field:{
+      height:SCREEN_HEIGHT * 0.045,borderWidth:0.5,borderColor:'grey',fontSize:20,width: SCREEN_WIDTH * 0.26,left: 150, borderRadius: 20,
+    },
+    amountStyle:{
+      flexDirection: 'row',flexWrap: 'wrap',
+      flex:2,
+            },
+   amount1Style:{
+     flexDirection:'column', flex:1, justifyContent:'space-between', marginBottom:20,
+    //  backgroundColor:'red',
+   },
+   amount2Style:{
+     flexDirection:'column',flex:1,
+    //  marginBottom:10,
+   },
+   incStyle:{
+     flexDirection:'row', justifyContent:'space-evenly',
+   },
     slugStyle:{
-    
-      fontSize:18,
-      padding:20,
-      textAlign:"left",
-      color:'red',
-      
-    
-  },
+      fontSize:18, padding:20, textAlign:"left", color:'red',
+    },
   rateStyle:{
-    fontSize:20,
-    textAlign:'center',
-  },
+    fontSize:25,textAlign:'center',fontWeight:'bold',
+    },
   center:{
-    height:'100%',
+    height:'35%', 
     width:'100%',
+    // backgroundColor:'red',
+    flexDirection: 'row',justifyContent:"space-between",
+    
     // backgroundColor: 'grey',
     // justifyContent:'center'
     // backgroundColor:'blue',
   },
   top1:{
-    height:'10%',
+    height:'30%',
     // width:'100%',
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -238,9 +261,11 @@ const styles = StyleSheet.create({
   },
   stockStyle:{
     fontSize:15,
-    padding:20,
-    textAlign:"right",
-     alignItems:"flex-end",
+    paddingTop:20,
+     textAlign:"right",
+      alignItems:"flex-end",
+     color:'red',
+    
   },
     image1:{
       height: 300,
@@ -248,7 +273,7 @@ const styles = StyleSheet.create({
       flex:1,
         },
         timeStyle:{
-          fontSize:15,
+          fontSize:20,
           padding:10,
           textAlign:'center',
        
@@ -268,18 +293,19 @@ const styles = StyleSheet.create({
           // backgroundColor: 'red' , 
         },
         chocoStyle: {
-          fontSize:25,
+          fontSize:30,
           padding:20,
           color:'black',
           fontWeight: 'bold',
         },
     inc_style: {
         borderWidth:1,
+        
         borderColor:'rgba(0,0,0,0.2)',
         alignItems:'flex-end',
         
         
-        justifyContent:'flex-end',
+        // justifyContent:'space-between',
         width:30,
         height:30,
         backgroundColor:'#fff',
@@ -289,13 +315,16 @@ const styles = StyleSheet.create({
           borderWidth:1,
           borderColor:'rgba(0,0,0,0.2)',
           alignItems:'flex-end',
-        
-          justifyContent:'flex-end',
+         
+          // justifyContent:'space-between',
           width:30,
           height:30,
           backgroundColor:'#fff',
           borderRadius:50,
           },
+      order_button:{
+        height:SCREEN_HEIGHT * 0.07,width: SCREEN_WIDTH * 0.4,left: 120, borderRadius: 40,backgroundColor: 'blue',alignItems: 'center',padding: 13,
+      }
 
 });
 
